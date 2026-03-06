ef55a62882de4ac200c89f266464abd30be105a5
Raghavendra S <158007359+raaghu2002@users.noreply.github.com>
2026-03-06 12:24:21 +0000

    Integrated api to form and waiting page

---
+++ src/services/apiServices.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const BASE_URL = "https://cuddly-trout-6996x94x699jhx54g-8082.app.github.dev/api";
+
+export const checkInVisitor = async (payload) => {
+    try {
+        const response = await axios.post(`${BASE_URL}/visitors/checkin`, payload, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+
+export const checkOutVisitor = async (id) => {
+    try {
+        const response = await axios.put(`${BASE_URL}/visitors/${id}/checkout`, null, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
+
+export const getWaitingVisitors = async () => {
+    try {
+        const response = await axios.get(`${BASE_URL}/visitors/waiting`, {
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
+};
